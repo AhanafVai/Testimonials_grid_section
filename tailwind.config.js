d5f892ef1698817cd9ffc97c9375df4b9eb9420a
ahanaf <ahanafabdullah9@gmail.com>
2022-08-22 06:55:47 +0600

    mobile design complete

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      md: '375px',
+      xl: '1440px'
+    },
+    extend: {
+      fontFamily: {
+        barlow: "'Barlow Semi Condensed',serif"
+      },
+      backgroundImage: {
+        danielPattern: "url('images/bg-pattern-quotation.svg')"
+      },
+      colors: {
+        moderateViolet: 'hsl(263, 55%, 52%)',
+        veryDarkGrayishBlue: 'hsl(217, 19%, 35%)',
+        veryDarkBlackishBlue: 'hsl(219, 29%, 14%)',
+        White: ' hsl(0, 0%, 100%)',
+        lightGray: 'hsl(0, 0%, 81%)',
+        lightGrayishBlue: 'hsl(210, 46%, 95%)'
+      }
+    }
+  },
+  plugins: []
+}
